790ef27aa37b5755d17379fa9163ffd2a8f2ed5e
Patrick Madden <maddenpj@gmail.com>
2012-12-18 12:25:12 -0800

    Renaming error fix

--- Client.Tests/ReadTests.cs
+++ Client.Tests/ReadTests.cs
@@ -97,7 +97,7 @@ namespace Client.Tests
             }
 
             [Test]
-            public void RequestUrl()
+            public void _Id()
             {
                 var mockclient = TestCommon.GetMockRestClient<DataSet>(new DataSet());
                 var client = TestCommon.GetClient(mockclient.Object);
