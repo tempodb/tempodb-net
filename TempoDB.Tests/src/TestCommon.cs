74dabb73a9d0b413d457f97d7c18a80685b1c3e4
Mike Yagley <myagley@gmail.com>
2013-02-06 16:54:20 -0600

    Add RestSharp test helpers

--- TempoDB.Tests/src/TestCommon.cs
+++ TempoDB.Tests/src/TestCommon.cs
@@ -1,14 +1,36 @@
+using Moq;
 using NUnit.Framework;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace TempoDB.Tests
 {
-    [TestFixture]
-    public class TestCommon
+    public static class TestCommon
     {
-        [Test]
-        public void TestExample()
+        public static TempoDB GetClient(RestClient restClient)
         {
-            Assert.AreEqual(true, true);
+            return new TempoDB("api-key", "api-secret", client: restClient);
+        }
+
+        public static Mock<RestClient> GetMockRestClient(RestResponse response)
+        {
+            var client = new Mock<RestClient>();
+            client.Setup(cl => cl.Execute(It.IsAny<RestRequest>())).Returns(response);
+            return client;
+        }
+
+        public static bool ContainsParameter(IList<Parameter> parameters, string name, string value)
+        {
+            IEnumerable<Parameter> enumerable = parameters;
+            return enumerable.Any(parameter => parameter.Name.ToString() == name && parameter.Value.ToString() == value);
+        }
+
+        public static bool ContainsParameterByPattern(IList<Parameter> parameters, string name, string value)
+        {
+            IEnumerable<Parameter> enumerable = parameters;
+            return enumerable.Any(parameter => parameter.Name.ToString() == name && parameter.Value.ToString().Contains(value));
         }
     }
 }
